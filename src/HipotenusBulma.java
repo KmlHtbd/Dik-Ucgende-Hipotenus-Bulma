53c988a5a0e6c25c20881ec7b00aa652ada5e76f
kml htb <kmlhtbdev@gmail.com>
2022-10-03 19:55:41 +0300

    İlk commit

---
+++ src/HipotenusBulma.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class HipotenusBulma {
+    public static void main(String[] args) {
+        double a,b,c;
+        Scanner input = new Scanner (System.in);
+        System.out.print("Birinci Dik Kenarı Giriniz:");
+        a = input.nextDouble();
+        System.out.print("İkinci Dik Kenarı Giriniz:");
+        b = input.nextDouble();
+        c = Math.sqrt(a*a + b*b);
+        System.out.print("Hipotenüs:"+c);
+
+
+
+
+
+
+
+
+    }
+}
